e763731c153814029bf5790a959a2ef711594917
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-04-27 14:36:02 +0900

    すべてのサイトのHTTPヘルスチェックを行うように改良

--- src/checkWebStatus.js
+++ src/checkWebStatus.js
@@ -1,54 +1,67 @@
 const https = require('https');
 
 /**
- * サーバー監視対象サイトのステータスをチェックする
- * @param url // 監視対象サイトのURL
- * @returns
+ * 監視対象のウェブサイトのHTTPステータスをチェックする関数
+ * @param {string} url 監視するURL
+ * @param {number} timeout HTTP接続タイムアウト
+ * @returns {Promise<object>} HTTPステータスコードの情報が組み込まれたPromiseオブジェクト
  */
-function browseWebsite(url) {
+function browseWebsite(url, timeout = 5000) {
     return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            if (res.statusCode >= 200 && res.statusCode < 300) {
+        const options = {
+            method: 'HEAD',
+            timeout,
+        };
+        https
+            .request(url, options, (res) => {
+                const { statusCode } = res;
                 resolve({
-                    "success": true,
-                    "message": "OK"
+                    success: statusCode >= 200 && statusCode < 300,
+                    statusCode,
                 });
-            } else {
-                console.log("There was an error in accessing this website. Error Code:" + res.statusCode);
+            })
+            .on('error', (err) => {
                 reject({
-                    "success": false,
-                    "message": "There was an error in accessing this website.",
-                    "code": res.statusCode
+                    success: false,
+                    statusCode: 0,
+                    error: err,
                 });
-            }
-        }).on('error', (err) => {
-            console.log("There was an error in accessing this website.");
-            reject({
-                "success": false,
-                "message": "There was an error in accessing this website.",
-                "error": err
-            });
-        });
+            })
+            .end();
     });
 }
 
-// 監視が必要なサイトのリスト
 const siteList = [
     'https://www.pachinkovista.com/pfactory/model_top.php',
     'https://www.operal.jp/login/index/',
     'https://www.fscorp.jp/',
     'https://www.j-net-gs.com/gsession/common/cmn001.do',
     'https://www.kenbaiki-pro.jp/',
-]
+];
 
+/**
+ * サイトリストを順番に処理する関数
+ * @param {string[]} siteList 監視するURLの配列
+ * @returns {Promise<object[]>} 各サイトの結果が含まれたPromiseオブジェクト
+ */
 function executeSiteListSequentially(siteList) {
-    let promiseChain = Promise.resolve();
-    siteList.forEach((site) => {
-        promiseChain = promiseChain
-            .then(() => browseWebsite(site))
-            .then((result) => console.log(`Site ${site} check: ${result.message}`))
-            .catch((error) => console.error(`Site ${site} check: ${error.message}`));
+    const promiseArray = siteList.map((site) => {
+        return browseWebsite(site)
+            .then((result) => {
+                console.log(`Site ${site} check: ${result.success ? 'OK' : 'Error'}`);
+                return result;
+            })
+            .catch((error) => {
+                console.error(`Site ${site} check: ${error.error ? error.error.message : error.message}`);
+                return error;
+            });
     });
+
+    return Promise.all(promiseArray);
 }
 
-executeSiteListSequentially(siteList);
+executeSiteListSequentially(siteList).then((result) => {
+    console.log('All sites checked.');
+    console.log(result);
+})
+
